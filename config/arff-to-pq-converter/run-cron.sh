1aa488a0b3d4bb73836d96054af6ed25b432c19a
Jos van der Velde <j.d.v.d.velde@tue.nl>
2025-01-17 19:26:51 +0100

    Initial commit for read-only server

--- config/arff-to-pq-converter/run-cron.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-printenv | grep -v HOME >> /etc/environment
-
-touch /home/unprivileged-user/cron.log
-chown -R unprivileged-user:unprivileged-user /home/unprivileged-user
-/usr/sbin/cron -l 4 && tail -f /home/unprivileged-user/cron.log
