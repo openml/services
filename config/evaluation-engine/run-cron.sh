456586a261183abaf6145425693aa242a1056202
PGijsbers <p.gijsbers@tue.nl>
2026-01-28 17:19:02 +0100

    Update configuration to work with local database and localhost

--- config/evaluation-engine/run-cron.sh
+++ config/evaluation-engine/run-cron.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# We need to remove the default 127.0.0.1 localhost map to
+# ensure the remap to the static nginx ip address is respected.
+sed -i '/^127.0.0.1.*localhost/d' /etc/hosts
+
 printenv | grep -v HOME >> /etc/environment
 
 touch /cron.log
