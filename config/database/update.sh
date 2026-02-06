99f2e0a9cf4e6ffd26e1dee65d19d2c465278f6d
PGijsbers <p.gijsbers@tue.nl>
2026-02-06 13:29:28 +0200

    Add minio prefix to dataset path

--- config/database/update.sh
+++ config/database/update.sh
@@ -2,7 +2,7 @@
 # Change the filepath of openml.file 
 # from "https://www.openml.org/data/download/1666876/phpFsFYVN"
 # to "http://minio:9000/datasets/0000/0001/phpFsFYVN"
-mysql -hdatabase -uroot -pok -e 'UPDATE openml.file SET filepath = CONCAT("http://localhost:8000/datasets/0000/", LPAD(id, 4, "0"), "/", SUBSTRING_INDEX(filepath, "/", -1)) WHERE extension="arff";'
+mysql -hdatabase -uroot -pok -e 'UPDATE openml.file SET filepath = CONCAT("http://localhost:8000/minio/datasets/0000/", LPAD(id, 4, "0"), "/", SUBSTRING_INDEX(filepath, "/", -1)) WHERE extension="arff";'
 
 # Update openml.expdb.dataset with the same url
 mysql -hdatabase -uroot -pok -e 'UPDATE openml_expdb.dataset DS, openml.file FL SET DS.url = FL.filepath WHERE DS.did = FL.id;'
